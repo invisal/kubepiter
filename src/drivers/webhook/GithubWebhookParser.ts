5feb0aed9bafd85e8dbde1a0db6ff42df7820950
Visal .In <invisal@gmail.com>
2022-08-17 19:31:05 +0700

    Fixing Github webhook parser

--- src/drivers/webhook/GithubWebhookParser.ts
+++ src/drivers/webhook/GithubWebhookParser.ts
@@ -1,38 +1,32 @@
-import express from 'express'
-import {
-  GitWebhookDetail,
-  GitWebhookEvent,
-  GitWebhookParserInterface
-} from './GitWebhookParserInterface'
+import express from 'express';
+import { GitWebhookDetail, GitWebhookEvent, GitWebhookParserInterface } from './GitWebhookParserInterface';
 
 interface GithubPayloadBody {
   ref: string;
 }
 
 export default class GithubWebhookParser extends GitWebhookParserInterface {
-  parse (req: express.Request): GitWebhookDetail {
+  parse(req: express.Request): GitWebhookDetail {
     const body: GithubPayloadBody =
-      req.headers['content-type'] === 'application/json'
-        ? req.body
-        : JSON.parse(req.body.payload)
+      req.headers['content-type'] === 'application/json' ? req.body : JSON.parse(req.body.payload);
 
     // Parsing branch
-    const branch = body.ref.split('/').pop()
+    const branch = body.ref.split('/').pop();
 
     // Parsing event
-    const originalEvent = req.headers['X-Github-Event']
-    let event: GitWebhookEvent
-    if (originalEvent === 'push') {
-      event = GitWebhookEvent.PUSH
+    const originalEvent = req.headers['x-github-event'] as string;
+    let event: GitWebhookEvent;
+    if (originalEvent.toLowerCase() === 'push') {
+      event = GitWebhookEvent.PUSH;
     }
 
     return {
       branch,
-      event
-    }
+      event,
+    };
   }
 
-  detect (req: express.Request): boolean {
-    return req.headers['user-agent'].indexOf('Github') >= 0
+  detect(req: express.Request): boolean {
+    return req.headers['user-agent'].toLowerCase().indexOf('github') >= 0;
   }
 }
