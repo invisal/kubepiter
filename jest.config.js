e5994ac57a66450317b0c06578f37afba515c292
Visal .In <invisal@gmail.com>
2022-08-26 23:15:11 +0700

    Fixing the jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  roots: ['<rootDir>/src'],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleDirectories: ['node_modules', '<rootDir>'],
 };
